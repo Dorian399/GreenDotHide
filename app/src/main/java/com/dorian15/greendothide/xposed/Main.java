b4eb8186a821fd3565ef7560e9d09c34186294c5
Dorian399 <dorianetflorian@gmaiL.com>
2025-09-24 12:05:40 +0200

    Add logs for when a method is hooked

--- app/src/main/java/com/dorian15/greendothide/xposed/Main.java
+++ app/src/main/java/com/dorian15/greendothide/xposed/Main.java
@@ -2,6 +2,7 @@ package com.dorian15.greendothide.xposed;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
@@ -10,20 +11,24 @@ public class Main implements IXposedHookLoadPackage {
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
         if (!lpparam.packageName.equals("com.android.systemui"))
             return;
-
-        XposedHelpers.findAndHookMethod(
-                "com.android.systemui.statusbar.privacy.MiuiPrivacyControllerImpl",
-                lpparam.classLoader,
-                "setStatus",
-                int.class,
-                String.class,
-                android.os.Bundle.class,
-                new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        param.setResult(null);
+        try{
+            XposedHelpers.findAndHookMethod(
+                    "com.android.systemui.statusbar.privacy.MiuiPrivacyControllerImpl",
+                    lpparam.classLoader,
+                    "setStatus",
+                    int.class,
+                    String.class,
+                    android.os.Bundle.class,
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(null);
+                        }
                     }
-                }
-        );
+            );
+            XposedBridge.log("[GreenDotHide] Hooked into: com.android.systemui.statusbar.privacy.MiuiPrivacyControllerImpl.SetStatus");
+        }catch(Exception e){
+            XposedBridge.log("[GreenDotHide] Ignoring hook: com.android.systemui.statusbar.privacy.MiuiPrivacyControllerImpl.SetStatus");
+        }
     }
 }
