639622e4ccdac8c76a3ec98044d0d89d4dfbe48a
Dorian399 <dorianetflorian@gmaiL.com>
2026-03-28 13:18:35 +0100

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.dorian15.greendothide"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
